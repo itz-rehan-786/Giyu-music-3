ea935e56aef4d9c8f798e2f51a65951769dde7ab
Rehan Khan <151330013+itz-rehan-786@users.noreply.github.com>
2024-08-16 08:40:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Learningbots79/Learning_Bots",
+    "https://github.com/itz-rehan-786/Giyu-music-3.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
