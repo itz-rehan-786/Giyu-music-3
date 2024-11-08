bca24798342ec183314a1dd2b9e58c5cffcb9354
Rehan Khan <151330013+itz-rehan-786@users.noreply.github.com>
2024-11-08 11:32:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = "e911a7e37e4733ff4a1ca4fbacc4065e"
 BOT_TOKEN = "8125428412:AAE5kFCQO8JA9A1Wgrb3F4Wi3Y1_FWRNJzU"
 MONGO_DB_URI = "mongodb+srv://wangling01:wangling@cluster0.trgu2ty.mongodb.net/?retryWrites=true&w=majority"
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
-LOG_GROUP_ID = -1002195714255
+LOG_GROUP_ID =-1002195714255
 OWNER_ID = 7775259302
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
